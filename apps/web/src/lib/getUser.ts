d2139737f0c29b5e032573677855b1f06932527d
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-08-28 13:02:34 +0530

    feat(store): add session management with Zustand and user state handling

---
+++ apps/web/src/lib/getUser.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { jwtVerify } from "jose";
+import { IUser } from "@/types/user.type";
+
+export const getUser = async (): Promise<IUser | null> => {
+  const cookie = await cookies();
+
+  const accessToken = cookie.get("__matrio.atk")?.value;
+
+  if (!accessToken) return null;
+
+  try {
+    const { payload } = await jwtVerify(
+      accessToken,
+      new TextEncoder().encode(process.env.JWT_SECRET),
+    );
+    return payload as unknown as IUser;
+  } catch (error) {
+    return null;
+  }
+};
