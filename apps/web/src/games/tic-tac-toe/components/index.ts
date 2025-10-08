316228e3667fe77d81e1ab93799f0d19a177690f
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2025-10-08 12:13:05 +0530

    refactor: remove old tic-tac-toe directory structure
    - Remove legacy tic-tac-toe components from games/tic-tac-toe/
    - Remove old app routing structure for tic-tac-toe game
    - Prepare for new modular game architecture

--- apps/web/src/games/tic-tac-toe/components/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./TTTAiGame";
-export * from "./TTTBoard";
-export * from "./TTTGameTips";
-export * from "./TTTHeader";
-export * from "./TTTMenuButton";
-export * from "./TTTScoreCard";
--- apps/web/src/games/tic-tac-toe/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./useAiTTT";
-export * from "./useLocalTTT";
