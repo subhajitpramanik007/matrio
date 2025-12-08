1777cc30522a5a5bbe561b9a636177884f64b8ad
subhajitpramanik007 <subho.p.1011@gmail.com>
2025-12-08 19:43:04 +0530

    feat(checkers): enhance and fix checkers engine logic

--- apps/socket-server/src/games/checkers/checkers.engine.ts
+++ apps/socket-server/src/games/checkers/checkers.engine.ts
@@ -1,5 +1,5 @@
 import { PlayerID } from '../../core/player'
-import { GameRulesException } from '../../core/utils'
+import { GameRulesException, Logger } from '../../core/utils'
 import { CHECKERS_PIECE_MOVE_DIRECTION } from './checkers.constant'
 import {
     TCheckersBoard,
@@ -11,18 +11,31 @@ import {
 } from './checkers.type'
 
 export class CheckersEngine {
+    private logger = new Logger('CheckersEngine')
     private board: TCheckersBoard
     private turn: TCheckersPieceColor
     private boardSize: number
 
-    constructor(size: number, startingTurn: TCheckersPieceColor) {
-        if (size % 2 !== 0 || size <= 6) {
-            throw new GameRulesException('Board size must be an even number greater than 6')
-        }
+    constructor(size: number, startingTurn: TCheckersPieceColor = 'red') {
+        this.validateBoardSize(size)
+
         this.boardSize = size
         this.board = this.createEmptyBoard(size)
         this.initPiecesDefault()
         this.turn = startingTurn
+    }
+
+    /**
+     * Check checkers board size
+     */
+    private validateBoardSize(size: number) {
+        if (size < 6) throw new GameRulesException('Board size must be greater than 6')
+        if (size > 13) throw new GameRulesException('Board size must be less than 13')
+        if (size % 2 !== 0) throw new GameRulesException('Board size must be an even number')
+    }
+
+    resetBoardAsEmptyCells() {
+        this.board = this.createEmptyBoard(this.boardSize)
     }
 
     /* ------- Utilities ------- */
@@ -87,7 +100,7 @@ export class CheckersEngine {
         for (let row = 0; row < rowsPerPlayer; row++) {
             for (let col = 0; col < this.boardSize; col++) {
                 if (this.isDark([row, col])) continue
-                const piece = this.createCheckersPiece(row, col, 'red')
+                const piece = this.createCheckersPiece(row, col, 'black')
                 this.setPiece([row, col], piece)
             }
         }
@@ -96,7 +109,7 @@ export class CheckersEngine {
         for (let row = this.boardSize - rowsPerPlayer; row < this.boardSize; row++) {
             for (let col = 0; col < this.boardSize; col++) {
                 if (this.isDark([row, col])) continue
-                const piece = this.createCheckersPiece(row, col, 'black')
+                const piece = this.createCheckersPiece(row, col, 'red')
                 this.setPiece([row, col], piece)
             }
         }
@@ -127,12 +140,13 @@ export class CheckersEngine {
               ]
     }
 
-    private simulateMoveFrom([row, col]: TCheckersPosition): TCheckersMove[] {
+    simulateMoveFrom([row, col]: TCheckersPosition): TCheckersMove[] {
         const piece = this.getPiece([row, col])
         if (!piece) return []
         const moves: TCheckersMove[] = []
 
-        for (const [dRow, dCol] of this.directionsForPiece(piece)) {
+        const dirs = this.directionsForPiece(piece)
+        for (const [dRow, dCol] of dirs) {
             const to: TCheckersPosition = [row + dRow, col + dCol]
             if (!this.inBounds(to)) continue
             if (this.getPiece(to) === null) {
@@ -142,7 +156,25 @@ export class CheckersEngine {
         return moves
     }
 
-    private captureSequencesFrom(startingPos: TCheckersPosition): TCheckersMove[] {
+    /**
+     * Get all possible captures from a position
+     * - visit each square and get all possible captures from that square
+     * - Get piece at current square if it exists then:
+     *   - Get all possible directions for the piece
+     *   - For each direction:
+     *       - Get the middle and landing squares
+     *       - If the middle square is empty then skip
+     *       - If the landing square is empty then skip
+     *       - Get the piece at the landing square
+     *       - If the piece at the landing square is not your piece then skip
+     *       - Call dfs on the landing square
+     *   - If dfs returns any captures then add them to the list
+     * - Return the list
+     *
+     * @param startingPos - {TCheckersPosition}
+     * @returns TCheckersMove[]
+     */
+    captureSequencesFrom(startingPos: TCheckersPosition): TCheckersMove[] {
         const rootPiece = this.getPiece(startingPos)
         if (!rootPiece) return []
         const moves: TCheckersMove[] = []
@@ -165,9 +197,7 @@ export class CheckersEngine {
                 if (midPiece && midPiece.color !== piece.color && !landingPiece) {
                     foundCapture = true
 
-                    const nextBoardSnapshot: TCheckersBoard = boardSnapshot.map((row) =>
-                        row.map((cell) => ({ ...cell })),
-                    )
+                    const nextBoardSnapshot = boardSnapshot.map((row) => row.map((cell) => ({ ...cell })))
                     nextBoardSnapshot[midPos[0]][midPos[1]]!.piece = null
                     nextBoardSnapshot[landingPos[0]][landingPos[1]]!.piece = nextBoardSnapshot[pos[0]][pos[1]]!.piece
                     nextBoardSnapshot[pos[0]][pos[1]]!.piece = null
@@ -180,12 +210,13 @@ export class CheckersEngine {
                         nextBoardSnapshot[landingPos[0]][landingPos[1]]!.piece!.isKing = true
                     }
 
-                    dfs(landingPos, nextBoardSnapshot, [...captures, landingPos])
+                    dfs(landingPos, nextBoardSnapshot, [...captures, midPos])
                 }
             }
 
             if (!foundCapture) {
-                moves.push({ from: startingPos, to: pos, captures, isCapture: true })
+                // no more captures found then add move
+                if (captures.length > 0) moves.push({ from: startingPos, to: pos, captures, isCapture: true })
             }
         }
 
@@ -194,22 +225,37 @@ export class CheckersEngine {
         return moves
     }
 
+    /**
+     * Get all possible moves for a checkers piece
+     * - visit each square and get all possible moves
+     * - if piece is not your piece then skip
+     * - if piece is your piece then get captures and simple moves
+     * - add captures and simple moves
+     * - if captures moves are empty then return simple moves
+     *
+     * @param color{TCheckersPieceColor}
+     * @returns TCheckersMove[]
+     */
     getAllPossibleMovesFor(color: TCheckersPieceColor): TCheckersMove[] {
         const captures: TCheckersMove[] = []
         const simpleMoves: TCheckersMove[] = []
 
         for (let row = 0; row < this.boardSize; row++) {
             for (let col = 0; col < this.boardSize; col++) {
+                // get piece
                 const piece = this.getPiece([row, col])
+                // skip if no piece or not your piece
                 if (!piece || piece.color !== color) continue
 
                 const from: TCheckersPosition = [row, col]
 
+                // get captures and if any captures then add
                 const caps = this.captureSequencesFrom(from)
                 if (caps.length > 0) {
                     captures.push(...caps)
                 }
 
+                // get simple moves and if any simple moves then add
                 const simpleMovesFrom = this.simulateMoveFrom(from)
                 if (simpleMovesFrom.length > 0) {
                     simpleMoves.push(...simpleMovesFrom)
@@ -225,10 +271,11 @@ export class CheckersEngine {
     validateMove(move: TCheckersMove, playerId?: PlayerID): TCheckersMove {
         const piece = this.getPiece(move.from)
         if (!piece) throw new GameRulesException('Piece not found at start position')
-        if (piece.color !== playerId) throw new GameRulesException('Not your turn')
+        if (piece.color !== this.turn) throw new GameRulesException('Not your turn')
 
-        const isYourPiece = playerId && piece.ownerId && playerId === piece.ownerId
-        if (!isYourPiece) throw new GameRulesException('Piece does not belong to you')
+        // TODO: check if piece is your piece
+        // const isYourPiece = playerId && piece.ownerId && playerId === piece.ownerId
+        // if (!isYourPiece) throw new GameRulesException('Piece does not belong to you')
 
         const allValidMoves = this.getAllPossibleMovesFor(piece.color)
         const captureExists = allValidMoves.some((m) => m.isCapture)
@@ -274,6 +321,8 @@ export class CheckersEngine {
     applyMove(move: TCheckersMove, playerId?: PlayerID) {
         const canonicalMove = this.validateMove(move, playerId)
 
+        this.printBoard('Before move')
+
         const piece = this.getPiece(canonicalMove.from)
         this.setPiece(canonicalMove.from, null)
         this.setPiece(canonicalMove.to, { ...piece! })
@@ -285,7 +334,7 @@ export class CheckersEngine {
         const landingRow = canonicalMove.to[0]
         const becomeKing =
             !piece!.isKing &&
-            (piece!.moveDirection === 'forward' ? landingRow === 0 : landingRow === this.boardSize - 1)
+            (piece!.moveDirection === 'backward' ? landingRow === 0 : landingRow === this.boardSize - 1)
         if (becomeKing) {
             const piece = this.getPiece(canonicalMove.to)
             this.setPiece(canonicalMove.to, { ...piece!, isKing: true })
@@ -294,6 +343,8 @@ export class CheckersEngine {
         this.swapTurn()
 
         const winner = this.checkWinner()
+
+        this.printBoard('After Move')
 
         return { winner }
     }
@@ -342,11 +393,11 @@ export class CheckersEngine {
     }
 
     /* ------- Debugging ------- */
-    printBoard() {
-        let output = ''
+    printBoard(text?: string) {
+        let output = text ? `${text}\n` : ''
         for (const row of this.board) {
             for (const cell of row) {
-                output += cell.piece ? cell.piece.color[0] : ' '
+                output += cell.piece ? cell.piece.color[0] : '_'
             }
             output += '\n'
         }
