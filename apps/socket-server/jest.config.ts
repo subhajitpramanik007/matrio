39fec6d0d2365b52939b28ab0b64f968be9f3a44
subhajitpramanik007 <subho.p.1011@gmail.com>
2025-12-17 08:41:44 +0530

    feat(socket): update Jest configuration and add module path mappings

--- apps/socket-server/jest.config.ts
+++ apps/socket-server/jest.config.ts
@@ -1,21 +1,30 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
+import { createRequire } from 'module'
+import { pathsToModuleNameMapper } from 'ts-jest'
+
+const require = createRequire(import.meta.url)
+const { compilerOptions } = require('./tsconfig.json')
+
 export default {
     preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
     extensionsToTreatAsEsm: ['.ts'],
     moduleNameMapper: {
+        ...pathsToModuleNameMapper(compilerOptions.paths, {
+            prefix: '<rootDir>/',
+        }),
         '^(\\.{1,2}/.*)\\.js$': '$1',
-        '^(\\.{1,2}/.*)\\.ts$': '$1',
+        // '^(\\.{1,2}/.*)\\.ts$': '$1',
     },
     transform: {
         '^.+\\.tsx?$': [
             'ts-jest',
             {
                 useESM: true,
-                tsconfig: {
-                    module: 'ES2022',
-                    target: 'ES2022',
-                },
+                // tsconfig: {
+                //     module: 'NodeNext',
+                //     target: 'ES2020',
+                // },
             },
         ],
     },
