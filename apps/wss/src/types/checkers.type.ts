4956960ac22dc094a9850020d9c68d50abeb8b5f
subhajitpramanik007 <subho.p.1011@gmail.com>
2025-12-26 10:19:08 +0530

    Clean up old wss app

--- apps/wss/src/types/checkers.type.ts
+++
@@ -1,6 +0,0 @@
-import { GameType } from "./game.type";
-import { RoomCode, RoomId } from "./room.type";
-
-export type TCheckersPieceColor = "red" | "black";
-export type TCheckersCellPosition = [row: number, col: number];
-export type TCheckersRoomId = RoomId<GameType.CHECKERS, RoomCode>;
